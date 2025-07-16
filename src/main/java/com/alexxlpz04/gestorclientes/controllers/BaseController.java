35dce76b2c5d5a037d55171d5d2004911bb7a784
alejandro <alexlopezortega2004@gmail.com>
2025-07-16 17:20:32 +0200

    initial commit

---
+++ src/main/java/com/alexxlpz04/gestorclientes/controllers/BaseController.java
@@ -0,0 +1,12 @@
+package com.alexxlpz04.gestorclientes.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BaseController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
